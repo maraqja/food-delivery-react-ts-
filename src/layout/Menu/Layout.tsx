a05bc5910046980deefb5d439cb006368e43b782
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-04-21 15:55:51 +0300

    useLocation hook for mark active route

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,8 +1,16 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
+import { useEffect } from 'react';
+import cn from 'classnames';
 
 export function Layout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log(location);
+    }, [location]);
+
     return (
         <div className={styles['layout']}>
             <div className={styles['sidebar']}>
@@ -16,11 +24,21 @@ export function Layout() {
                     <div className={styles['email']}>kaka@kaka.com</div>
                 </div>
                 <div className={styles['menu']}>
-                    <Link to="/" className={styles['link']}>
+                    <Link
+                        to="/"
+                        className={cn(styles['link'], {
+                            [styles['active']]: location.pathname === '/',
+                        })}
+                    >
                         <img src="/menu-icon.svg" alt="Иконка меню" />
                         Menu
                     </Link>
-                    <Link to="/cart" className={styles['link']}>
+                    <Link
+                        to="/cart"
+                        className={cn(styles['link'], {
+                            [styles['active']]: location.pathname === '/cart',
+                        })}
+                    >
                         <img src="/cart-icon.svg" alt="Иконка корзины" />
                         Cart
                     </Link>
