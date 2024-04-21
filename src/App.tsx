d2dd7823bd4bbffeafb839244d298c2d53a0af7a
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-04-21 14:30:08 +0300

    routes in JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 function App() {
     const [counter, setCounter] = useState<number>(0);
 
@@ -16,6 +20,15 @@ function App() {
                 Big Кнопка
             </Button>
             <Input placeholder="Email" />
+            <div>
+                <a href="/">Menu</a>
+                <a href="/cart">Cart</a>
+            </div>
+            <Routes>
+                <Route path="/" element={<Menu />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
         </>
     );
 }
