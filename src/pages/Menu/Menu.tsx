48100456dbf98834484c9c98d848f0f2991c35b0
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-04-30 19:07:15 +0300

    get products with axios and add loading and error processing

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -5,20 +5,43 @@ import Search from '../../components/Search/Search';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
+import axios, { AxiosError } from 'axios';
+import { MenuList } from './MenuList/MenuList';
 
 export function Menu() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | undefined>();
 
     const getMenu = async () => {
+        // try {
+        //     const res = await fetch(`${PREFIX}/products`);
+        //     if (!res.ok) {
+        //         return;
+        //     }
+        //     const data = (await res.json()) as Product[];
+        //     setProducts(data);
+        // } catch (e) {
+        //     console.error(e);
+        //     return;
+        // }
         try {
-            const res = await fetch(`${PREFIX}/products`);
-            if (!res.ok) {
-                return;
-            }
-            const data = (await res.json()) as Product[];
+            setIsLoading(true);
+            await new Promise<void>((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, 2000)
+            );
+            const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
             setProducts(data);
+            setIsLoading(false);
         } catch (e) {
             console.error(e);
+            if (e instanceof AxiosError) {
+                setError(e.message);
+            }
+
+            setIsLoading(false);
             return;
         }
     };
@@ -33,17 +56,9 @@ export function Menu() {
                 <Search placeholder="Введите название" />
             </div>
             <div>
-                {products.map((p) => (
-                    <ProductCard
-                        key={p.id}
-                        id={p.id}
-                        name={p.name}
-                        description={p.ingredients.join(', ')}
-                        rating={p.rating}
-                        price={p.price}
-                        image={p.image}
-                    />
-                ))}
+                {error && <>{error}</>}
+                {!isLoading && <MenuList products={products} />}
+                {isLoading && <>Загрузка...</>}
             </div>
         </>
     );
