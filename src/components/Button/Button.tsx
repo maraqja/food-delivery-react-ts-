fe3e3592d7425e66dd0a6720722424b2700148cf
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-04-30 22:42:27 +0300

    login page

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,22 +2,45 @@ import { FC } from 'react';
 import './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
+import styles from './Button.module.css';
 
 export const ButtonAlt: FC<ButtonProps> = ({
     className,
     children,
+    appearence = 'small',
     ...props
 }) => {
     return (
-        <button className={cn('button accent', className)} {...props}>
+        <button
+            className={cn(
+                styles['button'],
+                styles['accent'],
+                styles[appearence],
+                className
+            )}
+            {...props}
+        >
             {children}
         </button>
     );
 };
 
-function Button({ children, className, ...props }: ButtonProps) {
+function Button({
+    children,
+    className,
+    appearence = 'small',
+    ...props
+}: ButtonProps) {
     return (
-        <button className={cn('button accent', className)} {...props}>
+        <button
+            className={cn(
+                styles['button'],
+                styles['accent'],
+                styles[appearence],
+                className
+            )}
+            {...props}
+        >
             {children}
         </button>
     );
