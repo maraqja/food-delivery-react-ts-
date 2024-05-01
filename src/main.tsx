5dc353e7fe2a2b388d70562670eb0506b307e12c
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-05-01 18:39:55 +0300

    start work with redux-toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
 import { Login } from './pages/Login/Login.tsx';
 import { Register } from './pages/Register/Register.tsx';
 import { RequireAuth } from './helpers/RequireAuth.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const Menu = lazy(() => import('./pages/Menu/Menu.tsx')); // теперь компонент Menu будет подгружаться только когда нужно
 
@@ -82,7 +84,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
-        {/* <App /> */}
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>
 );
