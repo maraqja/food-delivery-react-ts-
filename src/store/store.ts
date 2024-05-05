d42f8b58a4c98f7ad060e93044172707cf32a0f0
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-05-05 12:22:13 +0300

    persistent cart (saved in localStorage)

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice, { JWT_PERSISTENT_STATE } from './user.slice';
 import { saveState } from './storage';
-import cartSlice from './cart.slice';
+import cartSlice, { CART_PERSISTENT_STATE } from './cart.slice';
 
 export const store = configureStore({
     reducer: {
@@ -11,8 +11,9 @@ export const store = configureStore({
 });
 
 store.subscribe(() => {
-    saveState({ jwt: store.getState().user.jwt }, JWT_PERSISTENT_STATE);
-}); // в случае изменения состояния - записываем jwt в localStorage
+    saveState({ jwt: store.getState().user.jwt }, JWT_PERSISTENT_STATE); // в случае изменения состояния - записываем jwt в localStorage
+    saveState(store.getState().cart, CART_PERSISTENT_STATE);
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispath = typeof store.dispatch;
